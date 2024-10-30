738119a3ec5ea574f7f11a6bdde239f3789c6a9d
Ranjan Verma <ranjanverma@Ranjans-Air.lan>
2024-10-30 23:40:14 +0530

    Our first Express Server

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,15 @@
-console.log(10 * 5);
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello I am from your backend server</h1>");
+});
+
+app.get("/about", (req, res) => {
+  res.send("<h1>Hello I am from your backend about</h1>");
+});
+
+app.listen(8000, () => {
+  console.log("The port is listening on PORT 8000");
+});
