e056b6e7c60fd8a7af656152f46ab8d0bee0001f
Ranjan Verma <ranjanverma@Ranjans-Air.lan>
2024-11-22 15:50:45 +0530

    Hashing Password

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const userSchema = mongoose.Schema({
   name: {
@@ -16,6 +17,13 @@ const userSchema = mongoose.Schema({
     type: String,
     required: true,
   },
+});
+
+userSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+  next();
 });
 
 module.exports = mongoose.model("User", userSchema);
